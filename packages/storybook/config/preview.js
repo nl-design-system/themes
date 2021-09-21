312eec38a0415fff32ef501b38c460679dde319c
Robbert Broersma <git@robbertbroersma.nl>
2021-09-21 19:58:09 +0200

    feat: improve design tokens for Bodegraven-Reeuwijk

--- packages/storybook/config/preview.js
+++ packages/storybook/config/preview.js
@@ -5,6 +5,7 @@ import '@utrecht/component-library-css/dist/bem.css';
 import '@nl-design-system-unstable/amsterdam-design-tokens/dist/index.css';
 import '@nl-design-system-unstable/bodegraven-reeuwijk-design-tokens/dist/index.css';
 import '@nl-design-system-unstable/borne-design-tokens/dist/index.css';
+import '@nl-design-system-unstable/bodegraven-reeuwijk-design-tokens/src/font';
 import '@nl-design-system-unstable/drechterland-design-tokens/dist/index.css';
 // import '@nl-design-system-unstable/dso-toolkit-design-tokens/dist/index.css';
 import '@nl-design-system-unstable/duiven-design-tokens/dist/index.css';
---
+++ proprietary/bodegraven-reeuwijk-design-tokens/src/font.js
@@ -0,0 +1,2 @@
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/700.css';
