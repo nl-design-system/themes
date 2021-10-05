35257c75b2e088cb152d79c2a5ad7143b6365299
Robbert Broersma <git@robbertbroersma.nl>
2021-10-05 15:14:35 +0200

    fixup! feat: Rijkshuisstijl design tokens

--- proprietary/duo-design-tokens/style-dictionary.config.js
+++ proprietary/duo-design-tokens/style-dictionary.config.js
@@ -3,4 +3,8 @@ const { createConfig } = require('../../style-dictionary-config');
 
 module.exports = createConfig({
   selector: `.${config.prefix}-theme`,
+  source: [
+    '../../node_modules/@nl-design-system-unstable/rijkshuisstijl-design-tokens/src/brand/**/*.style-dictionary.json',
+    'src/**/*.style-dictionary.json',
+  ],
 });
