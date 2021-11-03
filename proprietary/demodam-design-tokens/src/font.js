9f223adfa15801375d3180f68b66d2d7da834a40
Yolijn <yolijn@frameless.io>
2021-11-03 16:47:17 +0100

    Avoid npm run clean during storybook build
    Solves the issues where webpack is trying to build precicely at the point where the packages are just deleted and not yet rebuild

---
+++ proprietary/demodam-design-tokens/src/font.js
@@ -0,0 +1,5 @@
+import '@fontsource/source-sans-pro/300.css';
+import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/400-italic.css';
+import '@fontsource/source-sans-pro/600.css';
+import '@fontsource/source-sans-pro/700.css';
