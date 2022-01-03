3848c232767749168bb85546248741fecb526e00
Yolijn <mail@yolijn.com>
2022-01-03 12:36:11 +0100

    ci: add upgrade scripts and configuration for patch minor and major

---
+++ .ncurc.major.js
@@ -0,0 +1,20 @@
+const minorConfig = require('./.ncurc.minor');
+
+module.exports = {
+  reject: [
+    ...minorConfig.reject,
+    // Percy needs a manual upgrade as it doesn't work without breaking
+    '@percy/storybook',
+    // Storybook needs React 16 to have support out of the box.
+    // Alternatively we could manually keep track of all sub-package versions,
+    // but the benefit of upgrading is currently not high enough.
+    'react',
+    'react-dom',
+    // Webpack 5 update can only happen after Storybook supports it, we'll need to do a manual upgrade then.
+    // For now keep the loader versions compatible with Webpack 4.
+    'css-loader',
+    'sass-loader',
+    'style-loader',
+    'webpack',
+  ],
+};
