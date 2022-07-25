5eca436c689eeda680c9223b2024441fcc7d144d
Yolijn <yolijn@frameless.io>
2022-07-25 12:52:14 +0200

    build: ignore updates for utrecht packages

--- .ncurc.patch.js
+++ .ncurc.patch.js
@@ -1,1 +1,8 @@
-module.exports = { reject: [] };
+module.exports = {
+  reject: [
+    '@utrecht/component-library-css',
+    '@utrecht/components',
+    '@utrecht/design-tokens',
+    '@utrecht/web-component-library-stencil',
+  ],
+};
