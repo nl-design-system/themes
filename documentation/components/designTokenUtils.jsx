b47c3196f6b89c49e708be24044fc8b56ec33fb0
Yolijn <mail@yolijn.com>
2022-09-06 05:39:37 +0200

    feat: render utrecht components with story-template

--- documentation/components/designTokenUtils.jsx
+++ documentation/components/designTokenUtils.jsx
@@ -1,31 +1,63 @@
-export const getTokens = (designTokens) => {
-  const filteredTokens = Object.values(designTokens).filter((token) => token.path && token.value);
-  const getControl = (syntax) => {
-    switch (syntax) {
-      case '<color>':
-        return 'color';
-      default:
-        return 'text';
-    }
-  };
+import { parse, convert } from 'pure-color';
 
-  return filteredTokens.map((token) => ({
-    name: `--${token.path.join('-')}`,
-    value: token.value,
-    control: getControl(token.css.syntax),
-    syntax: token.css.syntax,
-  }));
+const getControl = (syntax) => {
+  switch (syntax) {
+    case '<color>':
+      return 'color';
+    default:
+      return 'text';
+  }
+};
+
+const flattenTokens = (nestedTokens, prefix = '') => {
+  const flattened = {};
+
+  Object.keys(nestedTokens).forEach((token) => {
+    const key = prefix ? [prefix, token].join('.') : token;
+    const tokens = nestedTokens[token];
+
+    if (tokens.value || tokens.css) {
+      flattened[key] = tokens;
+    } else if (typeof tokens === 'object' && tokens !== null && !Array.isArray(tokens)) {
+      Object.assign(flattened, flattenTokens(tokens, key));
+    }
+  });
+
+  return flattened;
+};
+
+export const getTokens = (designTokens) => {
+  const filteredTokens = Object.values(flattenTokens(designTokens)).filter((token) => token.path && token.value);
+
+  return filteredTokens.map((token) => {
+    return {
+      name: `--${token.path.join('-')}`,
+      value: token.value.includes('hsl') ? convert.hsl.string(parse.hsl(token.value)) : token.value,
+      control: token.css && getControl(token.css.syntax),
+      syntax: token.css && token.css.syntax,
+      path: token.path,
+    };
+  });
 };
 
 export const getDesignTokenArgs = (tokens) => {
+  const syntaxTitle = {
+    '<color>': 'Color',
+    '<number>': 'Number',
+    '<length>': 'Length',
+    '*': '',
+  };
+
   return tokens.reduce((args, token) => {
     return {
       ...args,
       [token.name]: {
         control: token.control,
         table: {
+          category: 'Design Tokens',
+          subcategory: token && syntaxTitle[token.syntax || '*'],
           type: {
-            summary: token.syntax,
+            summary: token && token.path.join('.'),
           },
         },
       },
