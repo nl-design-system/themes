85a8014a4eeb0005697fe8c2cc285a7f400ee21f
Robbert Broersma <git@robbertbroersma.nl>
2022-10-02 22:55:30 +0200

    chore: add script to download fonts for testing locally

---
+++ proprietary/rotterdam-design-tokens/src/font/download.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+curl -o ArialforRotterdam.eot https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam.eot
+curl -o ArialforRotterdam.woff https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam.woff
+curl -o ArialforRotterdam.ttf https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam.ttf
+curl -o ArialforRotterdam.svg https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam.svg
+curl -o ArialforRotterdam-Bold.eot https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam-Bold.eot
+curl -o ArialforRotterdam-Bold.woff https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam-Bold.woff
+curl -o ArialforRotterdam-Bold.ttf https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam-Bold.ttf
+curl -o ArialforRotterdam-Bold.svg https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/ArialforRotterdam-Bold.svg
+curl -o Bolder-Bold.woff2 https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-Bold.woff2
+curl -o Bolder-Bold.woff https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-Bold.woff
+curl -o Bolder-Light.woff2 https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-Light.woff2
+curl -o Bolder-Light.woff https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-Light.woff
+curl -o Bolder-RegularItalic.woff2 https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-RegularItalic.woff2
+curl -o Bolder-RegularItalic.woff https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-RegularItalic.woff
+curl -o Bolder-Regular.woff2 https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-Regular.woff2
+curl -o Bolder-Regular.woff https://www.rotterdam.nl/rotterdam-files/resources/css/fonts/Bolder-Regular.woff
