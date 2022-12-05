9405c9d01a4635448a8518f772c44ad461a98f10
Robbert Broersma <git@robbertbroersma.nl>
2022-12-05 13:23:34 +0100

    style: start with Example theme Figma tokens

--- proprietary/haarlem-design-tokens/style-dictionary.config.js
+++ proprietary/haarlem-design-tokens/style-dictionary.config.js
@@ -2,5 +2,6 @@ const config = require('./src/config.json');
 const { createConfig } = require('../../style-dictionary-config');
 
 module.exports = createConfig({
+  source: ['figma/**/*tokens.json', 'figma/**/*.tokens.json', 'src/**/tokens.json', 'src/**/*.tokens.json'],
   selector: `.${config.prefix}-theme`,
 });
