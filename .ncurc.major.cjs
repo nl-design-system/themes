ccdfe1535c37ed7c50bcc1f21aecb33f9064a81a
matijs <matijs@gmail.com>
2024-05-15 16:57:11 +0200

    build: patch round 2024-05
    - install patch updates
    - install minor updates
    - install major updates

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -4,28 +4,11 @@ module.exports = {
   ...minorConfig,
   reject: [
     ...minorConfig.reject,
-    // Percy needs a manual upgrade as it doesn't work without breaking
-    '@percy/storybook',
-    // Storybook needs React 16 to have support out of the box.
-    // Alternatively we could manually keep track of all sub-package versions,
-    // but the benefit of upgrading is currently not high enough.
-    'react',
-    'react-dom',
-    '@types/react',
-    '@types/react-dom',
-    // Webpack 5 update can only happen after Storybook supports it, we'll need to do a manual upgrade then.
-    // For now keep the loader versions compatible with Webpack 4.
-    'css-loader',
-    'sass-loader',
-    'style-loader',
-    'webpack',
-    // Color package uses ES6 syntax from v4+ which is not compatible with our setup
     'color',
-    'storybook',
-    '@storybook/*',
     '@fontsource/*',
     // Prettier 3 is async
     'prettier',
+    'eslint',
   ],
   target: 'latest',
 };
