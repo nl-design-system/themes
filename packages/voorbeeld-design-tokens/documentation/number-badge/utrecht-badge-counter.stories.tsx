404621dbff7e3550780a81fcff7adf48ed814589
scar055 <rowanvanlaar@gmail.com>
2024-06-26 14:53:44 +0200

    docs(voorbeeld-design-tokens): add <NumberBadge/> story

---
+++ packages/voorbeeld-design-tokens/documentation/number-badge/utrecht-badge-counter.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { BadgeCounter } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-badge-counter',
+  title: 'Components/Number badge/Utrecht',
+  component: BadgeCounter,
+  args: {
+    children: '9',
+  },
+} satisfies Meta<typeof BadgeCounter>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: {
+    theme: 'voorbeeld-theme',
+  },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: {
+    theme: 'utrecht-theme',
+  },
+};
