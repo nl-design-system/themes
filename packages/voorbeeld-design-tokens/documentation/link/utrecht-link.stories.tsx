62c8d0e7435311724b4290090ae4315e2911808d
Ali Amori Kadhim <ali@frameless.io>
2024-06-26 14:10:58 +0200

    docs(voorbeeld-design-tokens): add `<Link />` stories

---
+++ packages/voorbeeld-design-tokens/documentation/link/utrecht-link.stories.tsx
@@ -0,0 +1,95 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Link } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-link',
+  title: 'Components/Link/Utrecht',
+  component: Link,
+  args: {
+    children: 'Read more',
+    href: 'https://www.example.com',
+  },
+  argTypes: {
+    external: {
+      name: 'external',
+      type: { name: 'boolean', required: false },
+      table: {
+        category: 'API',
+        defaultValue: { summary: 'false' },
+      },
+    },
+    href: {
+      name: 'href',
+      type: { name: 'string', required: false },
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+    'aria-current': {
+      name: 'aria-current',
+      type: { name: 'string', required: false },
+      options: [undefined, 'page', 'step', 'location', 'date', 'time', 'true', 'false'],
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+    hrefLang: {
+      name: 'hrefLang',
+      type: { name: 'string', required: false },
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+    lang: {
+      name: 'lang',
+      type: { name: 'string', required: false },
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+    placeholder: {
+      name: 'placeholder',
+      type: { name: 'boolean', required: false },
+      table: {
+        category: 'API',
+        defaultValue: { summary: '' },
+      },
+    },
+    rel: {
+      name: 'rel',
+      type: { name: 'string', required: false },
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+    target: {
+      name: 'target',
+      type: { name: 'string', required: false },
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+  },
+} satisfies Meta<typeof Link>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: {
+    theme: 'voorbeeld-theme',
+  },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: {
+    theme: 'utrecht-theme',
+  },
+};
