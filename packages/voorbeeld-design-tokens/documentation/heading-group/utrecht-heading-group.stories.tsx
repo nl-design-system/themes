1c1f3c8d7f344654ff386d36961171f7a07a8e22
Ali Amori Kadhim <ali@frameless.io>
2024-06-26 11:05:32 +0200

    docs(voorbeeld-design-tokens): add `<HeadingGroup/>` story

---
+++ packages/voorbeeld-design-tokens/documentation/heading-group/utrecht-heading-group.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading1, HeadingGroup, Paragraph } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-heading-group',
+  title: 'Components/Heading group/Utrecht',
+  component: HeadingGroup,
+  args: {
+    children: (
+      <>
+        <Heading1>The Quick Brown Fox Jumps Over The Lazy Dog</Heading1>
+        <Paragraph>The Quick Brown Fox Jumps Over The Lazy Dog</Paragraph>
+      </>
+    ),
+  },
+} satisfies Meta<typeof HeadingGroup>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: {
+    theme: 'voorbeeld-theme',
+  },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: {
+    theme: 'utrecht-theme',
+  },
+};
