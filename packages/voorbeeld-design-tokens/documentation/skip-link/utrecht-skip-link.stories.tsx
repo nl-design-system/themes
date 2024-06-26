30b74b93f9719e527b87dae0e55ed04dd24135cb
Ali Amori Kadhim <ali@frameless.io>
2024-06-26 15:35:39 +0200

    docs(voorbeeld-design-tokens): add skip link stories

---
+++ packages/voorbeeld-design-tokens/documentation/skip-link/utrecht-skip-link.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { SkipLink } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  title: 'Components/Skip link/Utrecht',
+  id: 'utrecht-skip-link',
+  component: SkipLink,
+  argTypes: {
+    href: {
+      name: 'href',
+      type: { name: 'string', required: true },
+      table: {
+        category: 'HTML attribute',
+        defaultValue: { summary: '' },
+      },
+    },
+  },
+  render: (args) => (
+    <SkipLink className="utrecht-skip-link--focus utrecht-skip-link--visible" href="#main" {...args}>
+      Skip to main content
+    </SkipLink>
+  ),
+} satisfies Meta<typeof SkipLink>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: {
+    theme: 'voorbeeld-theme',
+  },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: {
+    theme: 'utrecht-theme',
+  },
+};
