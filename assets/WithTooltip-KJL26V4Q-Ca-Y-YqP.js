026f71a2392548d1ef0d2b4f67ab0684cb810708
Robbert <mail@robbertbroersma.nl>
2024-06-27 07:32:46 +0000

    Deploying to gh-pages from @ nl-design-system/themes@894c521d584c88a9c46dba868ae43f72c9019135 🚀

--- assets/WithTooltip-KJL26V4Q-Ca-Y-YqP.js
+++
@@ -1,1 +0,0 @@
-import{W as h,W as l,b as s}from"./index-BdjN606Q.js";import"./iframe-BqxsWyWi.js";import"../sb-preview/runtime.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BOkhicXD.js";import"./extends-CF3RwP-h.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
---
+++ assets/readme-EZWVn49k.js
@@ -0,0 +1,5 @@
+import{j as n}from"./jsx-runtime-X2b_N9AH.js";import{useMDXComponents as i}from"./index-CRbi4mQH.js";import{M as r,d as s}from"./index-HL4BTClC.js";import"./index-uCp2LrAq.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Bd98GSVC.js";import"../sb-preview/runtime.js";import"./index-BOkhicXD.js";import"./extends-CF3RwP-h.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";const a=`# NL Design System design tokens provincie Zuid-Holland
+
+These tokens have been obtained by using the [Figma Designs of Zuid-Holland](https://www.figma.com/file/pWIiNmzPDwYtPbYOQevpm1/PZH-Wapenfeiten?node-id=0%3A1) and analysing the [website of Zuid-Holland](https://wapenfeiten.zuid-holland.nl/).
+`;function o(t){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"Zuid-Holland/README"}),`
+`,n.jsx(s,{children:a})]})}function M(t={}){const{wrapper:e}={...i(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o()}export{M as default};
