8626bf6894dda24141d57242633c9c092be74bf9
matijs <matijs@gmail.com>
2024-07-09 10:44:20 +0200

    build: patch round 2024-07
    Clean up .ncurc.patch.cjs and allow updates again
    
    - install patch updates
    - install minor updates
    - install major updates
    
    Storybook makes use of PromiseWithResolvers, add "ESNext" to "lib" in
    theme-toolkit's `tsconfig.json`.

--- .ncurc.patch.cjs
+++ .ncurc.patch.cjs
@@ -1,15 +1,7 @@
 module.exports = {
   dep: ['dev', 'prod'],
   install: 'always',
-  reject: [
-    '@gemeente-denhaag/components-react',
-    '@gemeente-denhaag/design-tokens-components',
-    '@nl-design-system/component-index',
-    '@utrecht/component-library-css',
-    '@utrecht/components',
-    '@utrecht/design-tokens',
-    '@utrecht/web-component-library-stencil',
-  ],
+  reject: [],
   root: true,
   target: 'patch',
   upgrade: true,
