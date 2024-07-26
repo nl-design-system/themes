c2cbbc5163b2c8281af03d3964654206f9752924
Yolijn <yolijn@frameless.io>
2024-07-26 17:05:18 +0200

    feat: add utrecht blockquote

---
+++ packages/voorbeeld-design-tokens/documentation/blockquote/utrecht-blockquote.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Blockquote, Paragraph } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-blockquote',
+  title: 'Components/Blockquote/Utrecht',
+  component: Blockquote,
+  parameters: { actions: { disable: true } },
+  args: {
+    children: (
+      <Paragraph className="utrecht-blockquote__content">The Quick Brown Fox Jumps Over The Lazy Dog</Paragraph>
+    ),
+  },
+} satisfies Meta<typeof Blockquote>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: { theme: 'utrecht-theme' },
+};
