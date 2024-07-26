ecf8ace9714a4b3dce7caa14c76aca9a4dd72ba1
Yolijn <yolijn@frameless.io>
2024-07-26 16:38:02 +0200

    feat: add utrecht accordion

---
+++ packages/voorbeeld-design-tokens/documentation/accordion/utrecht-accordion.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AccordionProvider } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-accordion',
+  title: 'Components/Accordion/Utrecht',
+  component: AccordionProvider,
+  args: {
+    sections: [
+      {
+        headingLevel: 2,
+        label: 'Lorem ipsum 1',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.`,
+      },
+      {
+        headingLevel: 2,
+        label: 'Lorem ipsum 2',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.`,
+      },
+      {
+        headingLevel: 2,
+        label: 'Lorem ipsum 3',
+        body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+      laborum.`,
+      },
+    ],
+  },
+} satisfies Meta<typeof AccordionProvider>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: { theme: 'utrecht-theme' },
+};
