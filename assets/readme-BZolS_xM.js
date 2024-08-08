7f8f2732a1f4d218c57d0315fa93694a1ff20c9c
Robbert <mail@robbertbroersma.nl>
2024-08-08 12:21:29 +0000

    Deploying to gh-pages from @ nl-design-system/themes@a34a768a0dc93b8e95bd9877fe4d0daea9981c1d 🚀

--- assets/readme-BZolS_xM.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as s}from"./index-BI1Biiay.js";import{ae as m,ak as r}from"./index-ARythXBZ.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CX6f2YxO.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const a=`# Design Tokens for Amsterdam Design System
-
-Information for the tokens have been obtained from [inside the \`amsterdam-styled-component\` package](https://github.com/Amsterdam/amsterdam-styled-components/tree/master/packages/asc-ui/src/theme/default), the documentation for [Amsterdam Design System](http://amsterdam.nl/designsystem/) and the [designs in Figma](<https://www.figma.com/file/ORa7CBIooPgZj6HsEPBxNR/Design-bibliotheek-(gepubliceerd)>).
-
-## File format
-
-The tokens have been stored in a JSON file that follows the [Style Dictionary 3.0](https://amzn.github.io/style-dictionary/) format.
-`;function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Amsterdam/README"}),`
-`,t.jsx(r,{children:a})]})}function k(e={}){const{wrapper:n}={...s(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{k as default};
