f201e8e6af1d5dd65efe3b84a91299666a13b754
Robbert <mail@robbertbroersma.nl>
2024-08-15 14:32:56 +0000

    Deploying to gh-pages from @ nl-design-system/themes@652bd124bf6436a865fbd565c64f9ebd455a48b5 🚀

---
+++ assets/color-Dki79Gr-.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as s}from"./index-mYX3x30Z.js";import{t as n}from"./tokens-B-XcfGbs.js";import{C as p,a as c}from"./ColorSearch-BQHhFN6k.js";import{c as e}from"./config-aBAX7SSW.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-w7rTlB9c.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index-BQ5IbGbl.js";import"./index-BXPc1jzK.js";import"./clsx-B-dksMZM.js";function i(r){const t={h1:"h1",h2:"h2",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Nijmegen/Color"}),`
+`,o.jsx(t.h1,{id:"color",children:"Color"}),`
+`,o.jsx(t.h2,{id:"find-a-color",children:"Find a color"}),`
+`,o.jsx(p,{tokens:n[e.prefix].color}),`
+`,o.jsx(t.h2,{id:"color-palette",children:"Color palette"}),`
+`,o.jsx(c,{tokens:n[e.prefix].color})]})}function w(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{w as default};
