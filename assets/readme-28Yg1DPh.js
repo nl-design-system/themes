0861c91da2b4882db91afd8625f810c0df420727
Robbert <mail@robbertbroersma.nl>
2024-08-17 11:55:49 +0000

    Deploying to gh-pages from @ nl-design-system/themes@c2d8a46791724d2e2af6d109238dc25cd7e6127c 🚀

---
+++ assets/readme-28Yg1DPh.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-BLv1EH3b.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-AG-f3xX7.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Vught
+
+These tokens have been obtained by analysing the [website of Gemeente Vught](https://www.vught.nl).
+`;function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Vught/README"}),`
+`,t.jsx(i,{children:m})]})}function w(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{w as default};
