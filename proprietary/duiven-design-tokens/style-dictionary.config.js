c5299b0568ba4118e79bf9b348e0f5df232b4c7b
Robbert Broersma <git@robbertbroersma.nl>
2024-08-25 20:24:51 +0200

    feat: new design token package layout

--- proprietary/duiven-design-tokens/style-dictionary.config.js
+++ proprietary/duiven-design-tokens/style-dictionary.config.js
@@ -2,6 +2,7 @@ const config = require('./src/config.json');
 const { createConfig } = require('../../style-dictionary-config');
 
 module.exports = createConfig({
+  backwardsCompatible: true,
   selector: `.${config.prefix}-theme`,
   source: ['../riddeliemers-design-tokens/src/**/*.tokens.json', 'src/**/*.tokens.json'],
 });
