a8abd380b04b0bc5d5bb477f9515a573b0808d2c
Robbert <mail@robbertbroersma.nl>
2024-09-01 10:27:16 +0000

    Deploying to gh-pages from @ nl-design-system/themes@7be8d9feaea7f6d9efbdfc285ef05cbc4f6730bf 🚀

---
+++ assets/readme-Dvn0UwJT.js
@@ -0,0 +1,5 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-ByN5tj0E.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CojeiwdW.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Zwolle
+
+These tokens have been obtained by analysing the [website of Gemeente Zwolle](https://www.zwolle.nl).
+`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Zwolle/README"}),`
+`,e.jsx(i,{children:m})]})}function M(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{M as default};
