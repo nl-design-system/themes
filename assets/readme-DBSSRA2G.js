a879583408ba73cd2bfaf9434dfc289fcdc045d5
Robbert <mail@robbertbroersma.nl>
2024-09-06 15:47:09 +0000

    Deploying to gh-pages from @ nl-design-system/themes@b6034856966906f43923bccad29ce1f5feca17c5 🚀

--- assets/readme-DBSSRA2G.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as s}from"./index-lynCWzqw.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cj0maMeU.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Tilburg
-
-These tokens have been obtained by analysing the [website of Gemeente Tilburg](https://www.tilburg.nl).
-`;function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Tilburg/README"}),`
-`,t.jsx(s,{children:m})]})}function j(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{j as default};
