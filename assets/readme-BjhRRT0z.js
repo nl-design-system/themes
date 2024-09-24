57bec08df9de93cbda500bb179eea3a2e58ecf5d
Robbert <mail@robbertbroersma.nl>
2024-09-24 13:36:54 +0000

    Deploying to gh-pages from @ nl-design-system/themes@6354076dc418faa9da43e5162d0b143b7d2103dd 🚀

---
+++ assets/readme-BjhRRT0z.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as o,ak as r}from"./index-DLmvjwH4.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Dv8B-Hyc.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Duiven
+
+These tokens have been obtained by analysing the [website of Gemeente Duiven](https://www.duiven.nl).
+
+## RID de Liemers
+
+Gemeente Duiven is associated with [RID de Liemers](https://riddeliemers.nl/). Because of this collaboration the following websites share many design decisions:
+
+- [Gemeente Duiven](https://www.duiven.nl)
+- [Gemeente Westervoort](https://www.westervoort.nl)
+- [Gemeente Zevenaar](https://www.zevenaar.nl)
+`;function s(n){return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Duiven/README"}),`
+`,e.jsx(r,{children:m})]})}function x(n={}){const{wrapper:t}={...i(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(s,{...n})}):s()}export{x as default};
---
+++ assets/readme-CHn_e9Y3.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as e}from"./index-BI1Biiay.js";import{ae as i,ak as s}from"./index-DLmvjwH4.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Dv8B-Hyc.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const a=`# Design Tokens for Basic Layer
+
+## File format
+
+The tokens have been stored in a JSON file that follows the [Style Dictionary 3.0](https://amzn.github.io/style-dictionary/) format.
+`;function r(o){return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Basis/README"}),`
+`,t.jsx(s,{children:a})]})}function y(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r()}export{y as default};
