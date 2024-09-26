0fbef8e44e46ba1dfc9f0acef8c87a30f8ce6889
Robbert <mail@robbertbroersma.nl>
2024-09-26 09:45:33 +0000

    Deploying to gh-pages from @ nl-design-system/themes@1dd6f9b4e7e8cdb1ed91d426b222b069b1c4e6ce 🚀

--- assets/readme-XdA-szvp.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-DTBBgyxO.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DZa2Exvu.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens XXLLNC
-
-These tokens have been obtained by analysing the [website of XXLLNC](https://xxllnc.nl/).
-`;function o(n){return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"xxllnc/README"}),`
-`,t.jsx(i,{children:m})]})}function X(n={}){const{wrapper:e}={...r(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o()}export{X as default};
