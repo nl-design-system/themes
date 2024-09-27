de0e6e8ed36fc688eafefd7f556cda3e13bcbd70
Robbert <mail@robbertbroersma.nl>
2024-09-27 09:59:57 +0000

    Deploying to gh-pages from @ nl-design-system/themes@bc33236a564322054c716956478f645618cb0784 🚀

---
+++ assets/color-TxSx4bfd.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as s}from"./index-C-nTXHN9.js";import{t as n}from"./tokens-BcmdWk-s.js";import{C as p,a}from"./ColorSearch-SHKJm2Ft.js";import{c as e}from"./config-J8c96kmG.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B8F_QIOm.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index-Bxwjk3lc.js";import"./index-BQ5IbGbl.js";import"./design-tokens-Bd5iWdop.js";import"./index-BXPc1jzK.js";import"./clsx-B-dksMZM.js";function i(r){const t={h1:"h1",h2:"h2",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Demodam/Color"}),`
+`,o.jsx(t.h1,{id:"color",children:"Color"}),`
+`,o.jsx(t.h2,{id:"find-a-color",children:"Find a color"}),`
+`,o.jsx(p,{tokens:n[e.prefix].color}),`
+`,o.jsx(t.h2,{id:"color-palette",children:"Color palette"}),`
+`,o.jsx(a,{tokens:n[e.prefix].color})]})}function E(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{E as default};
