bce534a87d036a3f9f09c6f6a9dc6c57cea6b5c3
Robbert <mail@robbertbroersma.nl>
2024-09-28 09:11:15 +0000

    Deploying to gh-pages from @ nl-design-system/themes@5caf8a2d117c63751adbeae0f4014b0f36694825 🚀

---
+++ assets/readme-CSyHFuFs.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as e}from"./index-BI1Biiay.js";import{ae as m,ak as i}from"./index-CmgwUQYD.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-T3mNRhsP.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const s=`<!-- @license CC0-1.0 -->
+
+# TODO: write readme
+`;function o(r){return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Amsterdam/README"}),`
+`,t.jsx(i,{children:s})]})}function D(r={}){const{wrapper:n}={...e(),...r.components};return n?t.jsx(n,{...r,children:t.jsx(o,{...r})}):o()}export{D as default};
