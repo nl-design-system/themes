7310575316d4998da503156040483cb1ff33fd48
Robbert <mail@robbertbroersma.nl>
2024-10-04 08:44:56 +0000

    Deploying to gh-pages from @ nl-design-system/themes@9ec5b3f1bb3fe3351a3266b65b27242557fa7b57 🚀

--- assets/color-QO_6QCyQ.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as s}from"./index-D5E9xVmh.js";import{C as p,a}from"./ColorSearch-Cp6IhxuP.js";import{c as n}from"./config-CyqCWqZz.js";import{t as i}from"./tokens-Dv8RmXE_.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DDZT6l9K.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./index-BqQ5ME5e.js";import"./index-BQ5IbGbl.js";import"./design-tokens-BQrvzWkL.js";import"./index-CcSvOCRN.js";import"./clsx-B-dksMZM.js";import"./ExampleTokensCSS-BYZrbYPT.js";function e(r){const t={h1:"h1",h2:"h2",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Amsterdam/Color"}),`
-`,o.jsx(t.h1,{id:"color",children:"Color"}),`
-`,o.jsx(t.h2,{id:"find-a-color",children:"Find a color"}),`
-`,o.jsx(p,{tokens:i[n.prefix].color}),`
-`,o.jsx(t.h2,{id:"color-palette",children:"Color palette"}),`
-`,o.jsx(a,{tokens:i[n.prefix].color})]})}function E(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(e,{...r})}):e(r)}export{E as default};
