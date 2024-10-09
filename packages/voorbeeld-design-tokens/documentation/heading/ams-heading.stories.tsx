8f9f12edf9027af037dedfef3343ed919d25b962
Robbert Broersma <git@robbertbroersma.nl>
2024-10-09 18:47:48 +0200

    docs: add Amsterdam Heading component

---
+++ packages/voorbeeld-design-tokens/documentation/heading/ams-heading.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Heading } from '@amsterdam/design-system-react';
+
+const meta = {
+  id: 'ams-heading',
+  title: 'Components/Heading/Amsterdam',
+  component: Heading,
+  parameters: { actions: { disable: true } },
+  args: {
+    children: "Pa's wijze lynx bezag vroom het fikse aquaduct",
+    level: 1,
+  },
+  argTypes: {},
+} satisfies Meta<typeof Heading>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const AmsterdamTheme: Story = {
+  name: 'Amsterdam theme',
+  parameters: { theme: 'ams-theme' },
+};
