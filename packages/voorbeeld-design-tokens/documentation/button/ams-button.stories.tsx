3974eaea4d44ad21441df7269347d00527ce1b4b
Robbert Broersma <git@robbertbroersma.nl>
2024-10-09 18:47:33 +0200

    docs: add Amsterdam Button component

---
+++ packages/voorbeeld-design-tokens/documentation/button/ams-button.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@amsterdam/design-system-react';
+
+const meta = {
+  id: 'ams-button',
+  title: 'Components/Button/Amsterdam',
+  component: Button,
+  parameters: { actions: { disable: true } },
+  args: {
+    children: 'Button label',
+  },
+  argTypes: {},
+} satisfies Meta<typeof Button>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const AmsterdamTheme: Story = {
+  name: 'Amsterdam theme',
+  parameters: { theme: 'ams-theme' },
+};
