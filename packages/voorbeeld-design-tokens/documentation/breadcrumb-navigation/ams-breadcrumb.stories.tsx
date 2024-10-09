3d8253bd4992088b5cefbd5888ff5c68653f9b9a
Robbert Broersma <git@robbertbroersma.nl>
2024-10-09 18:47:25 +0200

    docs: add Amsterdam Breadcrumb component

---
+++ packages/voorbeeld-design-tokens/documentation/breadcrumb-navigation/ams-breadcrumb.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Breadcrumb } from '@amsterdam/design-system-react';
+
+const meta = {
+  id: 'ams-breadcrumb',
+  title: 'Components/Breadcrumb navigation/Amsterdam',
+  component: Breadcrumb,
+  parameters: { actions: { disable: true } },
+  args: {},
+  argTypes: {},
+  render: () => (
+    <Breadcrumb>
+      <Breadcrumb.Link href="#">Home</Breadcrumb.Link>
+      <Breadcrumb.Link href="#">Afval</Breadcrumb.Link>
+      <Breadcrumb.Link href="#">Bedrijfsafval</Breadcrumb.Link>
+      <Breadcrumb.Link href="#">Recyclepunten voor bedrijven</Breadcrumb.Link>
+    </Breadcrumb>
+  ),
+} satisfies Meta<typeof Breadcrumb>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const AmsterdamTheme: Story = {
+  name: 'Amsterdam theme',
+  parameters: { theme: 'ams-theme' },
+};
