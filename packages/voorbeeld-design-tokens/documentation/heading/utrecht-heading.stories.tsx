c78f693a06e0e5dedb4078dc968baa8561e43577
Robbert Broersma <git@robbertbroersma.nl>
2024-10-09 18:50:22 +0200

    feat: add Utrecht Heading component

---
+++ packages/voorbeeld-design-tokens/documentation/heading/utrecht-heading.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Heading } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-heading',
+  title: 'Components/Heading/Utrecht',
+  component: Heading,
+  parameters: { actions: { disable: true } },
+  args: {
+    children: "Pa's wijze lynx bezag vroom het fikse aquaduct",
+    level: 1,
+  },
+  argTypes: {},
+} satisfies Meta<typeof Heading>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: { theme: 'utrecht-theme' },
+};
