d465de7df4e57f2438d1e9c71062525b73fac786
Marwa <marwahello@frameless.io>
2024-10-18 14:06:38 +0200

    feat: add NORA tokens

---
+++ proprietary/nora-design-tokens /README.md
@@ -0,0 +1,3 @@
+# NL Design System design tokens Gemeente NORA
+
+These tokens have been obtained by analysing the [website of Gemeente NORA](https://gemeente.nora.nl).
---
+++ proprietary/nora-design-tokens /src/font.js
@@ -0,0 +1,4 @@
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400-italic.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/500-italic.css';
