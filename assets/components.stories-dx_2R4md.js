9fb4050769810364c956254cb32b54b8b33d1150
Robbert <mail@robbertbroersma.nl>
2024-10-20 21:34:15 +0000

    Deploying to gh-pages from @ nl-design-system/themes@1225996c47a3ad1bbc8aed1a49f06c4ef2f1e542 🚀

--- assets/components.stories-dx_2R4md.js
+++
@@ -1,5 +0,0 @@
-import{C as a}from"./ComponentStories-CWL55sHp.js";import{c as s}from"./config-Bn-cy9OA.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index.esm-BhC0IwuT.js";import"./clsx-B-dksMZM.js";import"./index-DTBbmC6q.js";import"./extends-CF3RwP-h.js";const u={title:"Basis",component:a,parameters:{controls:{include:"showAll"},chromatic:{disableSnapshot:!1}},args:{config:s,showAll:!1}},o={parameters:{theme:`${s.prefix}`}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  parameters: {
-    theme: \`\${config.prefix}\`
-  }
-}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const g=["Components"];export{o as Components,g as __namedExportsOrder,u as default};
---
+++ assets/typography-DLPHJCcr.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as a}from"./index-BI1Biiay.js";import{ae as p,af as r}from"./index-DteUfXFa.js";import{t as l}from"./tokens--jZtbr5b.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CaTovTW-.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const t=l.pzh.typography,s=Object.values(t.scale).map(n=>n["font-size"].value);function i(n){const o={a:"a",h1:"h1",h2:"h2",p:"p",...a(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{title:"Zuid-Holland/Typography"}),`
+`,`
+`,e.jsx(o.h1,{id:"typography",children:"Typography"}),`
+`,e.jsxs(o.p,{children:["See also: ",e.jsx(o.a,{href:"https://wapenfeiten.zuid-holland.nl/bouwstenen/lettertype/lettertypes-voor-vormgeving",rel:"nofollow",children:"Lettertypes voor vormgeving"}),"."]}),`
+`,e.jsx(o.h2,{id:"karbon",children:"Karbon"}),`
+`,e.jsx(o.p,{children:`Non-free font. Contact provincie Zuid-Holland if you need access and are developing for them.
+Sans serif font.`}),`
+`,e.jsx(r,{fontFamily:t["sans-serif"]["font-family"].value,fontSizes:s,sampleText:"The Quick Brown Fox Jumps Over The Lazy Dog"}),`
+`,e.jsx(r,{fontFamily:t["sans-serif"]["font-family"].value,fontSizes:s,fontWeight:600,sampleText:"The Quick Brown Fox Jumps Over The Lazy Dog"}),`
+`,e.jsx(o.h2,{id:"georgia",children:"Georgia"}),`
+`,e.jsx(o.p,{children:"Serif font. This serif font is used for subtitles and quotes."}),`
+`,e.jsx(r,{fontFamily:t.serif["font-family"].value,fontSizes:s,sampleText:"The Quick Brown Fox Jumps Over The Lazy Dog"})]})}function z(n={}){const{wrapper:o}={...a(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(i,{...n})}):i(n)}export{z as default,s as fontSizes,t as typography};
