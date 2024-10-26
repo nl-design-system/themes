7eda6de0c9790db25125e38ee1138b165231ada4
matijs <matijs@gmail.com>
2024-10-26 12:28:03 +0200

    chore: align @types/node with node version
    Align @types/node with the version of node that is used in the repo. Add
    a comment to update @types/node when the node version in the repo is
    bumped.
    
    Prevent npm-check-updates and Dependabot from bumping @types/node major
    versions.

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -4,6 +4,8 @@ module.exports = {
   ...minorConfig,
   reject: [
     ...minorConfig.reject,
+    // @types/node is kept in line with the node version in .nvmrc and package.json#engines.node
+    '@types/node',
     'color',
     '@fontsource/*',
     // Prettier 3 is async
