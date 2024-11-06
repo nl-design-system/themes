2e93371294d5534d7c30d5e1dbb4cf3191ed41f1
Robbert <mail@robbertbroersma.nl>
2024-11-06 09:06:05 +0000

    Deploying to gh-pages from @ nl-design-system/themes@36dae5d9bdbc94c83f9611f33d642f74a3aaf013 🚀

---
+++ assets/readme-CgyotkGU.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as a}from"./index-DLsJSwxI.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DZr_PIRG.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# Design Tokens for Haarlem Design System
+
+Information for the tokens have been obtained from the [haarlem.nl website](https://haarlem.nl) and the [designs in Figma](https://www.figma.com/file/yNP20OJZKRG3dW5dmDtyM9/Online-Huisstijl---Gemeente-Haarlem-en-Gemeente-Zandvoort?node-id=50%3A765&t=IzsJHOwbUrmak4wS-1).
+
+## File format
+
+The tokens have been stored in a JSON file that follows the [Style Dictionary 3.0](https://amzn.github.io/style-dictionary/) format.
+`;function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Haarlem/README"}),`
+`,t.jsx(a,{children:m})]})}function y(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{y as default};
