495b9e923e09002370b3bd0a815ceebff88436cc
Robbert <mail@robbertbroersma.nl>
2024-11-08 14:02:29 +0000

    Deploying to gh-pages from @ nl-design-system/themes@b7f4479a558af7111ddadf6cf36f4ab5b29a7b5c 🚀

--- assets/readme-BESVQPrd.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as s}from"./index-UI4ldCq8.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BuW605V2.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const a=`# NL Design System design tokens Gemeente Dinkelland
-
-These tokens have been obtained by analysing the [website of Gemeente Dinkelland](https://www.dinkelland.nl/).
-`;function o(n){return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Dinkelland/README"}),`
-`,e.jsx(s,{children:a})]})}function j(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{j as default};
