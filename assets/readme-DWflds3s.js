2a7f59e95f7d13011793f039570116102ff65adc
Robbert <mail@robbertbroersma.nl>
2024-11-08 13:52:33 +0000

    Deploying to gh-pages from @ nl-design-system/themes@b735e2595a147bd39467980e6d4e67179ff9c34f 🚀

---
+++ assets/readme-DWflds3s.js
@@ -0,0 +1,5 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-UI4ldCq8.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BuW605V2.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const a=`# NL Design System design tokens Gemeente Venray
+
+These tokens have been obtained by analysing the [website of Gemeente Venray](https://www.venray.nl).
+`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Venray/README"}),`
+`,e.jsx(i,{children:a})]})}function w(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{w as default};
---
+++ assets/readme-higm1xkS.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as m,ak as s}from"./index-UI4ldCq8.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BuW605V2.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const i=`# NL Design System design tokens for Demodam
+
+These tokens have been obtained by analysing the [website of Demodam](https://demodam.nl/).
+`;function n(e){return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Demodam/README"}),`
+`,t.jsx(s,{children:i})]})}function M(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(n,{...e})}):n()}export{M as default};
