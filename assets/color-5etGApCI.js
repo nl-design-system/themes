870e8fe8bf324c2581be14fa6216b8a032433e10
Robbert <mail@robbertbroersma.nl>
2024-11-11 15:42:06 +0000

    Deploying to gh-pages from @ nl-design-system/themes@4838b341da69cc1321328890140dc087caefad54 🚀

---
+++ assets/color-5etGApCI.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as s}from"./index-1hfjUwVL.js";import{t as n}from"./tokens--jZtbr5b.js";import{C as p,a as l}from"./ColorSearch-COJx6sU7.js";import{c as i}from"./config-ESjlItmb.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BnihGL2i.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./util-Bkb1mpYR.js";import"./index-BQ5IbGbl.js";import"./index-Bl0auJpn.js";import"./clsx-B-dksMZM.js";function e(r){const t={h1:"h1",h2:"h2",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Zuid-Holland/Color"}),`
+`,o.jsx(t.h1,{id:"color",children:"Color"}),`
+`,o.jsx(t.h2,{id:"find-a-color",children:"Find a color"}),`
+`,o.jsx(p,{tokens:n[i.prefix].color}),`
+`,o.jsx(t.h2,{id:"color-palette",children:"Color palette"}),`
+`,o.jsx(l,{tokens:n[i.prefix].color})]})}function y(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(e,{...r})}):e(r)}export{y as default};
