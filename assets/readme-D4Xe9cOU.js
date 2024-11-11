7f853441d50486ce770e2d391c858b500133d92e
Robbert <mail@robbertbroersma.nl>
2024-11-11 15:44:58 +0000

    Deploying to gh-pages from @ nl-design-system/themes@190e8d8b859856e9b7afba9cc503231a17f2b438 🚀

--- assets/readme-D4Xe9cOU.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-pwkwTHqH.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-IYjwwF4x.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Leidschendam Voorburg
-
-These tokens have been obtained by analysing the [website of Gemeente Leidschendam Voorburg](https://www.lv.nl).
-`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Leidschendam Voorburg/README"}),`
-`,e.jsx(i,{children:m})]})}function j(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{j as default};
