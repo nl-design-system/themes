6f31e7efef127ac33a0af9c6faf31cfafe82750e
Robbert Broersma <git@robbertbroersma.nl>
2024-11-12 10:34:08 +0100

    docs: add color sample component

---
+++ packages/voorbeeld-design-tokens/documentation/color-sample/utrecht-color-sample.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ColorSample } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-color-sample',
+  title: 'Components/Color Sample/Utrecht',
+  component: ColorSample,
+  args: {
+    color: '#007DAD',
+  },
+} satisfies Meta<typeof ColorSample>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: { theme: 'utrecht-theme' },
+};
