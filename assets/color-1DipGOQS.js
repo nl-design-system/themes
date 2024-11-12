578e9d34abaeb1a8fe2678ee7102be7d586cfdd6
matijs <github@matijsbrinkhuis.com>
2024-11-12 14:19:06 +0000

    Deploying to gh-pages from @ nl-design-system/themes@c956ad38f939aff17af77abaa94b49e96ce3ecb2 🚀

--- assets/color-1DipGOQS.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as s}from"./index-mqDbar23.js";import{t as n}from"./tokens-C2TnSKGa.js";import{C as p,a}from"./ColorSearch-BBelyCiz.js";import{c as e}from"./config-Y67l1f_v.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-gH3RKOuC.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./util-Bkb1mpYR.js";import"./index-BQ5IbGbl.js";import"./index-DKKFdtDf.js";import"./clsx-B-dksMZM.js";function i(r){const t={h1:"h1",h2:"h2",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Basis Thema/Color"}),`
-`,o.jsx(t.h1,{id:"color",children:"Color"}),`
-`,o.jsx(t.h2,{id:"find-a-color",children:"Find a color"}),`
-`,o.jsx(p,{tokens:n[e.prefix].color}),`
-`,o.jsx(t.h2,{id:"color-palette",children:"Color palette"}),`
-`,o.jsx(a,{tokens:n[e.prefix].color})]})}function w(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{w as default};
---
+++ assets/readme-R6Rcxu25.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-BPX24RHh.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DT8lfGFO.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Noordoostpolder
+
+These tokens have been obtained by analysing the [website of Gemeente Noordoostpolder](https://www.noordoostpolder.nl).
+`;function n(t){return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Noordoostpolder/README"}),`
+`,o.jsx(i,{children:m})]})}function M(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(n,{...t})}):n()}export{M as default};
