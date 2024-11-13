3eabecdd3e482c464aaea8074204d5ea84771705
matijs <github@matijsbrinkhuis.com>
2024-11-13 16:35:46 +0000

    Deploying to gh-pages from @ nl-design-system/themes@cda886c02e4537e8ef7552380c95a5c080265fda 🚀

---
+++ assets/readme-Bx85OKgq.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-B9JlBnK1.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CV7F5lKp.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Westervoort
+
+These tokens have been obtained by analysing the [website of Gemeente Westervoort](https://www.westervoort.nl).
+
+## RID de Liemers
+
+Gemeente Westervoort is associated with [RID de Liemers](https://riddeliemers.nl/). Because of this collaboration the following websites share many design decisions:
+
+- [Gemeente Duiven](https://www.duiven.nl)
+- [Gemeente Westervoort](https://www.westervoort.nl)
+- [Gemeente Zevenaar](https://www.zevenaar.nl)
+`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Westervoort/README"}),`
+`,e.jsx(i,{children:m})]})}function D(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{D as default};
