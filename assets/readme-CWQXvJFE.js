777344269536d7372f46b2bcc1f5dd1956f76cb8
Yolijn <mail@yolijn.com>
2024-12-05 21:07:15 +0000

    Deploying to gh-pages from @ nl-design-system/themes@938f32144fb8b6118803d61b6b18e7b119b5fe6e 🚀

---
+++ assets/readme-CWQXvJFE.js
@@ -0,0 +1,5 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-xq5VanUU.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CE5NSq1X.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Tubbergen
+
+These tokens have been obtained by analysing the [website of Gemeente Tubbergen](https://www.tubbergen.nl/).
+`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Tubbergen/README"}),`
+`,e.jsx(i,{children:m})]})}function l(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{l as default};
