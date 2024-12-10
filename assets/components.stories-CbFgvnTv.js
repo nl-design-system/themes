de5a4de509f52581af97a87368e3f4ba508487a9
Robbert <mail@robbertbroersma.nl>
2024-12-10 08:42:27 +0000

    Deploying to gh-pages from @ nl-design-system/themes@0585a93d9ac54d6314b911c438c7981c49eb8b68 🚀

--- assets/components.stories-CbFgvnTv.js
+++
@@ -1,5 +0,0 @@
-import{C as m}from"./ComponentStories-DEVJ0j86.js";import{c as s}from"./config-EfySG5c8.js";import{t as a}from"./list-BVfaOGSk.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index.esm-BhC0IwuT.js";import"./clsx-B-dksMZM.js";import"./index-Bpdomuim.js";const g={title:"Purmerend",component:m,parameters:{controls:{include:"showAll"},chromatic:{disableSnapshot:!1}},args:{config:s,showAll:!1,tokens:a}},e={args:{theme:"purmerend-theme purmerend-theme--media-query"}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    theme: 'purmerend-theme purmerend-theme--media-query'
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const C=["Components"];export{e as Components,C as __namedExportsOrder,g as default};
---
+++ assets/typography-2gC6W_6T.js
@@ -0,0 +1,10 @@
+import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as a}from"./index-BI1Biiay.js";import{ae as i,af as t}from"./index-BOfuWHFA.js";import{t as r}from"./tokens-DKWY22hl.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B3H0nZpR.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";function s(n){const e={a:"a",h1:"h1",h2:"h2",p:"p",...a(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Demodam/Typography"}),`
+`,o.jsx(e.h1,{id:"typography",children:"Typography"}),`
+`,o.jsx(e.h2,{id:"source-sans-pro",children:"Source Sans Pro"}),`
+`,o.jsx(e.p,{children:"Free font. Sans serif font. Default font weight is lighter at 300 and bold is 500."}),`
+`,o.jsx(t,{fontFamily:r.denbosch.typography["sans-serif"]["font-family"].value,fontSizes:["24px"],sampleText:"The Quick Brown Fox Jumps Over The Lazy Dog"}),`
+`,o.jsxs(e.p,{children:["See also: ",o.jsx(e.a,{href:"https://fonts.google.com/specimen/Source+Sans+Pro",rel:"nofollow",children:'"Source Sans Pro" on Google Fonts'}),"."]}),`
+`,o.jsx(e.h2,{id:"oranda",children:"Oranda"}),`
+`,o.jsx(e.p,{children:"Non-free font. Serif font. Font weight 600 is used for headings."}),`
+`,o.jsx(t,{fontFamily:r.denbosch.typography.serif["font-family"].value,fontSizes:["24px"],sampleText:"The Quick Brown Fox Jumps Over The Lazy Dog"}),`
+`,o.jsxs(e.p,{children:["See also: ",o.jsx(e.a,{href:"https://www.linotype.com/1085632/oranda-family.html",rel:"nofollow",children:'"Oranda" on Linotype'}),"."]})]})}function S(n={}){const{wrapper:e}={...a(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(s,{...n})}):s(n)}export{S as default};
