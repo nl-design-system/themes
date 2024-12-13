1543fa294c80242a2258204252ec7574af1e968f
Robbert <mail@robbertbroersma.nl>
2024-12-13 16:12:16 +0000

    Deploying to gh-pages from @ nl-design-system/themes@58ea5ac6db71d927fc175f8e1b44e646bf72d53d 🚀

--- assets/readme-CjOaEYzV.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as m,ak as s}from"./index-C3mvajX7.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-z4k89D_O.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const i=`# NL Design System design tokens for Demodam
-
-These tokens have been obtained by analysing the [website of Demodam](https://demodam.nl/).
-`;function n(e){return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Demodam/README"}),`
-`,t.jsx(s,{children:i})]})}function M(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(n,{...e})}):n()}export{M as default};
