44750f058afe2b52a1d59ed996241098cba31441
Robbert <mail@robbertbroersma.nl>
2024-12-18 11:16:47 +0000

    Deploying to gh-pages from @ nl-design-system/themes@e431845d4f6dfdfdf9ae8790cfca9e22e5663ee4 🚀

--- assets/readme-BqMa2NRu.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as s,ak as i}from"./index-FmbHW6Vp.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-FOt4uFDI.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Borne
-
-These tokens have been obtained by analysing the [website of Gemeente Borne](https://www.borne.nl).
-`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Borne/README"}),`
-`,e.jsx(i,{children:m})]})}function M(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o()}export{M as default};
