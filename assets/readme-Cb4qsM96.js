1304849a3a289058b5cb7c23b0f6c143c49841ad
Robbert <mail@robbertbroersma.nl>
2024-12-19 10:43:40 +0000

    Deploying to gh-pages from @ nl-design-system/themes@8d286a6fcc0dcee4b30ae47069e1c85cb291ea92 🚀

--- assets/readme-Cb4qsM96.js
+++
@@ -1,5 +0,0 @@
-import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as i}from"./index-BI1Biiay.js";import{ae as r,ak as s}from"./index-KChhtK6K.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-DFZbNIHe.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const a=`# NL Design System design tokens provincie Zuid-Holland
-
-These tokens have been obtained by using the [Figma Designs of Zuid-Holland](https://www.figma.com/file/pWIiNmzPDwYtPbYOQevpm1/PZH-Wapenfeiten?node-id=0%3A1) and analysing the [website of Zuid-Holland](https://wapenfeiten.zuid-holland.nl/).
-`;function o(t){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"Zuid-Holland/README"}),`
-`,n.jsx(s,{children:a})]})}function j(t={}){const{wrapper:e}={...i(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o()}export{j as default};
