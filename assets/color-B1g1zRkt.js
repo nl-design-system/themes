ae728944e190d4e64c952e79afe4f043e877f0b7
Robbert <mail@robbertbroersma.nl>
2024-12-19 10:30:06 +0000

    Deploying to gh-pages from @ nl-design-system/themes@f348ae9e39c64a68c40e2faa918ee7df1b2e2f29 🚀

--- assets/color-B1g1zRkt.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as s}from"./index-BqSUE6u8.js";import{t as n}from"./tokens-DYFrxH8t.js";import{C as p,a as c}from"./ColorSearch-BoQ0Yi5H.js";import{c as e}from"./config-EfySG5c8.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-CaGZsORh.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";import"./util-AfaQczwz.js";import"./index-CVs1w2sG.js";import"./index-BQ5IbGbl.js";import"./index-NspZYKNv.js";import"./clsx-B-dksMZM.js";function i(r){const t={h1:"h1",h2:"h2",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{title:"Purmerend/Color"}),`
-`,o.jsx(t.h1,{id:"color-xxx",children:"Color xxx"}),`
-`,o.jsx(t.h2,{id:"find-a-color",children:"Find a color"}),`
-`,o.jsx(p,{tokens:n[e.prefix].color}),`
-`,o.jsx(t.h2,{id:"color-palette",children:"Color palette"}),`
-`,o.jsx(c,{tokens:n[e.prefix].color})]})}function E(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{E as default};
