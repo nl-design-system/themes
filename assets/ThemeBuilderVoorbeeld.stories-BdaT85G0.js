0207653889da23a68c33d9a938346ea4c672dfd2
Robbert <mail@robbertbroersma.nl>
2024-12-20 10:56:13 +0000

    Deploying to gh-pages from @ nl-design-system/themes@39fe8499c929fafccd61e431b6407ed820dd276c 🚀

---
+++ assets/ThemeBuilderVoorbeeld.stories-BdaT85G0.js
@@ -0,0 +1,105 @@
+import{T as Xe,s as Ye,e as Ze,r as er,u as rr,d as sr}from"./ThemeBuilder-C_dYR1Su.js";import{t as ar}from"./tokens-D6Na0-EC.js";import{t as _}from"./tokens-H40ZFKRb.js";import{t as tr}from"./tokens-C1Qzpcek.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-IOQbIOJG.js";import"./objectWithoutProperties-eofjzj2c.js";import"./extends-CF3RwP-h.js";import"./clsx-B-dksMZM.js";import"./StyleDictionary-BxpJYSSB.js";import"./ExampleTokensCSS-CfgSc4Ci.js";import"./_baseSet-DWBlGqXY.js";import"./_Set-B09sQViy.js";import"./_setToArray-fgp7Mjkt.js";const xr={title:"Theme Builder/Voorbeeld Thema",id:"theme-builder",component:Xe,args:{step:0,steps:Ye,basis:ar,theme:_},argTypes:{example:{control:"select",options:Object.keys(Ze)},theme:{control:"select",options:["voorbeeld","groningen","denhaag","rotterdam","utrecht"],mapping:{voorbeeld:_,groningen:tr,rotterdam:er,utrecht:rr,denhaag:sr}}}},e={args:{step:0}},r={args:{step:1}},s={args:{step:2}},a={args:{step:3}},t={args:{step:4}},o={args:{step:5}},p={args:{step:6}},c={args:{step:7}},n={args:{step:8}},m={args:{step:9}},d={args:{step:10}},g={args:{step:11}},u={args:{step:12}},i={args:{step:13}},S={args:{step:14}},l={args:{step:15}},h={args:{step:16}},T={args:{step:17}},b={args:{step:18}},f={args:{step:19}},k={args:{step:20}},x={args:{step:21}},v={args:{step:22}},y={args:{step:23}},B={args:{step:24}},O={args:{step:25}};var $,j,E;e.parameters={...e.parameters,docs:{...($=e.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    step: 0
+  }
+}`,...(E=(j=e.parameters)==null?void 0:j.docs)==null?void 0:E.source}}};var V,q,w;r.parameters={...r.parameters,docs:{...(V=r.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    step: 1
+  }
+}`,...(w=(q=r.parameters)==null?void 0:q.docs)==null?void 0:w.source}}};var z,A,C;s.parameters={...s.parameters,docs:{...(z=s.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    step: 2
+  }
+}`,...(C=(A=s.parameters)==null?void 0:A.docs)==null?void 0:C.source}}};var D,F,G;a.parameters={...a.parameters,docs:{...(D=a.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    step: 3
+  }
+}`,...(G=(F=a.parameters)==null?void 0:F.docs)==null?void 0:G.source}}};var H,I,J;t.parameters={...t.parameters,docs:{...(H=t.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    step: 4
+  }
+}`,...(J=(I=t.parameters)==null?void 0:I.docs)==null?void 0:J.source}}};var K,L,M;o.parameters={...o.parameters,docs:{...(K=o.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    step: 5
+  }
+}`,...(M=(L=o.parameters)==null?void 0:L.docs)==null?void 0:M.source}}};var N,P,Q;p.parameters={...p.parameters,docs:{...(N=p.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    step: 6
+  }
+}`,...(Q=(P=p.parameters)==null?void 0:P.docs)==null?void 0:Q.source}}};var R,U,W;c.parameters={...c.parameters,docs:{...(R=c.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    step: 7
+  }
+}`,...(W=(U=c.parameters)==null?void 0:U.docs)==null?void 0:W.source}}};var X,Y,Z;n.parameters={...n.parameters,docs:{...(X=n.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    step: 8
+  }
+}`,...(Z=(Y=n.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var ee,re,se;m.parameters={...m.parameters,docs:{...(ee=m.parameters)==null?void 0:ee.docs,source:{originalSource:`{
+  args: {
+    step: 9
+  }
+}`,...(se=(re=m.parameters)==null?void 0:re.docs)==null?void 0:se.source}}};var ae,te,oe;d.parameters={...d.parameters,docs:{...(ae=d.parameters)==null?void 0:ae.docs,source:{originalSource:`{
+  args: {
+    step: 10
+  }
+}`,...(oe=(te=d.parameters)==null?void 0:te.docs)==null?void 0:oe.source}}};var pe,ce,ne;g.parameters={...g.parameters,docs:{...(pe=g.parameters)==null?void 0:pe.docs,source:{originalSource:`{
+  args: {
+    step: 11
+  }
+}`,...(ne=(ce=g.parameters)==null?void 0:ce.docs)==null?void 0:ne.source}}};var me,de,ge;u.parameters={...u.parameters,docs:{...(me=u.parameters)==null?void 0:me.docs,source:{originalSource:`{
+  args: {
+    step: 12
+  }
+}`,...(ge=(de=u.parameters)==null?void 0:de.docs)==null?void 0:ge.source}}};var ue,ie,Se;i.parameters={...i.parameters,docs:{...(ue=i.parameters)==null?void 0:ue.docs,source:{originalSource:`{
+  args: {
+    step: 13
+  }
+}`,...(Se=(ie=i.parameters)==null?void 0:ie.docs)==null?void 0:Se.source}}};var le,he,Te;S.parameters={...S.parameters,docs:{...(le=S.parameters)==null?void 0:le.docs,source:{originalSource:`{
+  args: {
+    step: 14
+  }
+}`,...(Te=(he=S.parameters)==null?void 0:he.docs)==null?void 0:Te.source}}};var be,fe,ke;l.parameters={...l.parameters,docs:{...(be=l.parameters)==null?void 0:be.docs,source:{originalSource:`{
+  args: {
+    step: 15
+  }
+}`,...(ke=(fe=l.parameters)==null?void 0:fe.docs)==null?void 0:ke.source}}};var xe,ve,ye;h.parameters={...h.parameters,docs:{...(xe=h.parameters)==null?void 0:xe.docs,source:{originalSource:`{
+  args: {
+    step: 16
+  }
+}`,...(ye=(ve=h.parameters)==null?void 0:ve.docs)==null?void 0:ye.source}}};var Be,Oe,_e;T.parameters={...T.parameters,docs:{...(Be=T.parameters)==null?void 0:Be.docs,source:{originalSource:`{
+  args: {
+    step: 17
+  }
+}`,...(_e=(Oe=T.parameters)==null?void 0:Oe.docs)==null?void 0:_e.source}}};var $e,je,Ee;b.parameters={...b.parameters,docs:{...($e=b.parameters)==null?void 0:$e.docs,source:{originalSource:`{
+  args: {
+    step: 18
+  }
+}`,...(Ee=(je=b.parameters)==null?void 0:je.docs)==null?void 0:Ee.source}}};var Ve,qe,we;f.parameters={...f.parameters,docs:{...(Ve=f.parameters)==null?void 0:Ve.docs,source:{originalSource:`{
+  args: {
+    step: 19
+  }
+}`,...(we=(qe=f.parameters)==null?void 0:qe.docs)==null?void 0:we.source}}};var ze,Ae,Ce;k.parameters={...k.parameters,docs:{...(ze=k.parameters)==null?void 0:ze.docs,source:{originalSource:`{
+  args: {
+    step: 20
+  }
+}`,...(Ce=(Ae=k.parameters)==null?void 0:Ae.docs)==null?void 0:Ce.source}}};var De,Fe,Ge;x.parameters={...x.parameters,docs:{...(De=x.parameters)==null?void 0:De.docs,source:{originalSource:`{
+  args: {
+    step: 21
+  }
+}`,...(Ge=(Fe=x.parameters)==null?void 0:Fe.docs)==null?void 0:Ge.source}}};var He,Ie,Je;v.parameters={...v.parameters,docs:{...(He=v.parameters)==null?void 0:He.docs,source:{originalSource:`{
+  args: {
+    step: 22
+  }
+}`,...(Je=(Ie=v.parameters)==null?void 0:Ie.docs)==null?void 0:Je.source}}};var Ke,Le,Me;y.parameters={...y.parameters,docs:{...(Ke=y.parameters)==null?void 0:Ke.docs,source:{originalSource:`{
+  args: {
+    step: 23
+  }
+}`,...(Me=(Le=y.parameters)==null?void 0:Le.docs)==null?void 0:Me.source}}};var Ne,Pe,Qe;B.parameters={...B.parameters,docs:{...(Ne=B.parameters)==null?void 0:Ne.docs,source:{originalSource:`{
+  args: {
+    step: 24
+  }
+}`,...(Qe=(Pe=B.parameters)==null?void 0:Pe.docs)==null?void 0:Qe.source}}};var Re,Ue,We;O.parameters={...O.parameters,docs:{...(Re=O.parameters)==null?void 0:Re.docs,source:{originalSource:`{
+  args: {
+    step: 25
+  }
+}`,...(We=(Ue=O.parameters)==null?void 0:Ue.docs)==null?void 0:We.source}}};const vr=["Step0","Step1","Step2","Step3","Step4","Step5","Step6","Step7","Step8","Step9","Step10","Step11","Step12","Step13","Step14","Step15","Step16","Step17","Step18","Step19","Step20","Step21","Step22","Step23","Step24","Step25"];export{e as Step0,r as Step1,d as Step10,g as Step11,u as Step12,i as Step13,S as Step14,l as Step15,h as Step16,T as Step17,b as Step18,f as Step19,s as Step2,k as Step20,x as Step21,v as Step22,y as Step23,B as Step24,O as Step25,a as Step3,t as Step4,o as Step5,p as Step6,c as Step7,n as Step8,m as Step9,vr as __namedExportsOrder,xr as default};
