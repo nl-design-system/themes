abc9b3e0d0e8049234944d71521d538722b694f7
Robbert <mail@robbertbroersma.nl>
2024-12-23 16:14:17 +0000

    Deploying to gh-pages from @ nl-design-system/themes@8e339a646f8acf11e166839aeef4994f89d4ad43 🚀

--- assets/components.stories-B1pSnaxc.js
+++
@@ -1,5 +0,0 @@
-import{C as m}from"./ComponentStories-BoZ--5KJ.js";import{c as s}from"./config-Bdfz0vo3.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index.esm-BhC0IwuT.js";import"./clsx-B-dksMZM.js";import"./index-NspZYKNv.js";const h={title:"Ridderkerk",component:m,parameters:{controls:{include:"showAll"},chromatic:{disableSnapshot:!1}},args:{config:s,showAll:!1}},e={parameters:{theme:`${s.prefix}-theme`}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  parameters: {
-    theme: \`\${config.prefix}-theme\`
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const u=["Components"];export{e as Components,u as __namedExportsOrder,h as default};
