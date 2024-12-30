c2ba744f710d2f9ac8d2e6f21af74ffee929bef4
Yolijn <mail@yolijn.com>
2024-12-30 12:56:07 +0000

    Deploying to gh-pages from @ nl-design-system/themes@122219d40bbc687680e186ed6fd951dd79d47c85 🚀

---
+++ assets/components.stories-C0fG4uv0.js
@@ -0,0 +1,5 @@
+import{C as s}from"./ComponentStories-CF2bJXlK.js";import{c as m}from"./config-J8c96kmG.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index.esm-BhC0IwuT.js";import"./clsx-B-dksMZM.js";import"./index-BJR50M2h.js";import"./util-Dtr1Ejjm.js";import"./css-DtK2WV3u.js";import"./objectDestructuringEmpty-kEsbOJzC.js";import"./extends-CF3RwP-h.js";const C={title:"Demodam",component:s,parameters:{controls:{include:"showAll"},chromatic:{disableSnapshot:!1}},args:{config:m,showAll:!1}},o={parameters:{theme:`${m.prefix}-theme`}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  parameters: {
+    theme: \`\${config.prefix}-theme\`
+  }
+}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const S=["Components"];export{o as Components,S as __namedExportsOrder,C as default};
--- assets/readme-CjvmGLYb.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as m}from"./index-DTJrOAl-.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-N4CcvW4g.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const s=`# NL Design System design tokens for Gemeente Nijmegen
-
-These tokens have been obtained by analysing the [website of Gemeente Nijmegen](https://www.nijmegen.nl) and the [Nijmegen Component Library](https://componenten.nijmegen.nl/).
-`;function o(n){return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Nijmegen/README"}),`
-`,e.jsx(m,{children:s})]})}function w(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o()}export{w as default};
