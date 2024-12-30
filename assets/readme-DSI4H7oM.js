bed2658dc5a4f49f31701b7232c7b683800157d7
Yolijn <mail@yolijn.com>
2024-12-30 09:47:33 +0000

    Deploying to gh-pages from @ nl-design-system/themes@4cd14a105f2440e28223441e6452254c9d4922ad 🚀

---
+++ assets/readme-DSI4H7oM.js
@@ -0,0 +1,5 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as m}from"./index-BI1Biiay.js";import{ae as o,ak as a}from"./index-DTJrOAl-.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-N4CcvW4g.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const s=`# NL Design System design tokens Gemeente Haarlemmermeer
+
+These tokens have been obtained by analysing the [website of Gemeente Haarlemmermeer](https://haarlemmermeergemeente.nl).
+`;function n(t){return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Haarlemmermeer/README"}),`
+`,e.jsx(a,{children:s})]})}function g(t={}){const{wrapper:r}={...m(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(n,{...t})}):n()}export{g as default};
