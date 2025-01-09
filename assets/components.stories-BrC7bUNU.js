f2cf488848f0f5373490490f07747800acaf6747
Robbert <mail@robbertbroersma.nl>
2025-01-09 14:14:48 +0000

    Deploying to gh-pages from @ nl-design-system/themes@28be97d8a48d807329e4a5d38a5cb3122bf04796 🚀

--- assets/components.stories-BrC7bUNU.js
+++
@@ -1,5 +0,0 @@
-import{C as s}from"./ComponentStories-CF2bJXlK.js";import{c as m}from"./config-aH7oMY8g.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index.esm-BhC0IwuT.js";import"./clsx-B-dksMZM.js";import"./index-BJR50M2h.js";import"./util-Dtr1Ejjm.js";import"./css-DtK2WV3u.js";import"./objectDestructuringEmpty-kEsbOJzC.js";import"./extends-CF3RwP-h.js";const C={title:"Noordwijk",component:s,parameters:{controls:{include:"showAll"},chromatic:{disableSnapshot:!1}},args:{config:m,showAll:!1}},o={parameters:{theme:`${m.prefix}-theme`}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  parameters: {
-    theme: \`\${config.prefix}-theme\`
-  }
-}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const w=["Components"];export{o as Components,w as __namedExportsOrder,C as default};
---
+++ assets/readme-DOXH43YH.js
@@ -0,0 +1,5 @@
+import{j as n}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as r}from"./index-BI1Biiay.js";import{ae as i,ak as s}from"./index-CzPhRL2U.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-BU3p5PBY.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-nrpnv_Jx.js";import"./index-DrFu-skq.js";const m=`# NL Design System design tokens Gemeente Groningen
+
+These tokens have been obtained by analysing the [website of Gemeente Groningen](https://gemeente.groningen.nl).
+`;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(i,{title:"Groningen/README"}),`
+`,n.jsx(s,{children:m})]})}function M(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o()}export{M as default};
