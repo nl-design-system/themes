42ebd18531cb2287f103857563525875bdb79538
mdubbe <mdubbe94@gmail.com>
2025-02-03 08:47:07 +0100

    Added Rijkshuisstijl stories Theme toegevoegd aan preview. Desing tokens pacakge geinstalleerd.

--- packages/storybook/config/preview.ts
+++ packages/storybook/config/preview.ts
@@ -4,6 +4,7 @@ import '@amsterdam/design-system-css/dist/index.css';
 import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
 import '@gemeente-rotterdam/design-tokens/dist/index.css';
 import '@nl-design-system-community/nora-design-tokens/dist/theme.css';
+import '@rijkshuisstijl-community/design-tokens/dist/index.css';
 import '@nl-design-system-community/nora-design-tokens/src/font.js';
 import '@nl-design-system-community/purmerend-design-tokens/dist/theme.css';
 import '@nl-design-system-community/purmerend-design-tokens/dist/color-scheme-dark/theme.css';
