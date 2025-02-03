df041d9c2dc13262e978d2c39b478cddfb192089
mdubbe <mdubbe94@gmail.com>
2025-02-03 08:47:07 +0100

    Added Rijkshuisstijl stories Theme toegevoegd aan preview. Desing tokens pacakge geinstalleerd.

---
+++ packages/theme-toolkit/src/component-stories-rhc.tsx
@@ -0,0 +1,12 @@
+import { STORY_GROUPS } from './component-stories-util';
+import { DotBadge } from '@rijkshuisstijl-community/components-react';
+
+export const RHC_COMPONENT_STORIES = [
+  {
+    storyId: 'react-rhc-dot-badge--default',
+    component: 'rhc-dot-badge',
+    group: STORY_GROUPS['BADGES'],
+    name: 'Rijkshuisstijl Dot Badge',
+    render: () => <DotBadge aria-label={'Nieuw bericht'} />,
+  },
+];
