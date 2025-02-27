801a7edd0c573055741657d50a69335fc40a201c
Mees <mdubbe@quintor.nl>
2025-02-27 22:58:19 +0100

    Rhc/dot badge (#942)
    Dot-badge component van het Rijkshuisstijl Community toegevoegd. 
    
    closes
    https://github.com/nl-design-system/rijkshuisstijl-community/issues/1089
    
    ---------
    
    Co-authored-by: Ruben Smit <rsmit@quintor.local>

---
+++ packages/voorbeeld-design-tokens/documentation/dot-badge/rhc-dot-badge.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DotBadge } from '@rijkshuisstijl-community/components-react';
+
+const meta = {
+  id: 'rhc-dot-badge',
+  title: 'Components/Dot Badge/Rijkshuisstijl community',
+  component: DotBadge,
+  parameters: { actions: { disable: true } },
+  args: {
+    label: 'Nieuw Bericht',
+  },
+} satisfies Meta<typeof DotBadge>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+export const RijkshuisstijlTheme: Story = {
+  name: 'Rijkshuisstijl theme',
+  parameters: { theme: 'rhc-theme' },
+};
