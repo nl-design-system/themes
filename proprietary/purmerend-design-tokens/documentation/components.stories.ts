998014cf669265f155a6215634c946b1b491eb9e
Robbert Broersma <git@robbertbroersma.nl>
2025-03-28 09:38:17 +0100

    refactor: split up Purmerend stories and re-enable Chromatic

--- proprietary/purmerend-design-tokens/documentation/components.stories.ts
+++ proprietary/purmerend-design-tokens/documentation/components.stories.ts
@@ -6,7 +6,7 @@ import tokens from '../dist/list.json';
 const meta = {
   title: 'Purmerend',
   component: ComponentStories,
-  parameters: { controls: { include: 'showAll' }, chromatic: { disableSnapshot: true } },
+  parameters: { controls: { include: 'showAll' }, chromatic: { disableSnapshot: false } },
   args: { config, showAll: false, tokens, displayDesignTokens: false },
 } satisfies Meta<typeof ComponentStories>;
 
@@ -15,15 +15,35 @@ type Story = StoryObj<typeof meta>;
 export default meta;
 
 export const ComponentsLightMode: Story = {
-  name: 'Visual regression test (color-scheme: light)',
+  name: 'Visual regression test (color-scheme: light, component 1 - 50)',
   args: {
     theme: 'purmerend-theme purmerend-theme--color-scheme-light',
+    start: 0,
+    end: 50,
+  },
+};
+
+export const ComponentsLightModeContinued: Story = {
+  name: 'Visual regression test (color-scheme: light, component 51 - ∞)',
+  args: {
+    theme: 'purmerend-theme purmerend-theme--color-scheme-light',
+    start: 51,
   },
 };
 
 export const ComponentsDarkMode: Story = {
-  name: 'Visual regression test (color-scheme: dark)',
+  name: 'Visual regression test (color-scheme: dark, component 1 - 50)',
   args: {
     theme: 'purmerend-theme purmerend-theme--color-scheme-dark',
+    start: 0,
+    end: 50,
+  },
+};
+
+export const ComponentsDarkModeContinued: Story = {
+  name: 'Visual regression test (color-scheme: dark, component 51 - ∞)',
+  args: {
+    theme: 'purmerend-theme purmerend-theme--color-scheme-dark',
+    start: 51,
   },
 };
