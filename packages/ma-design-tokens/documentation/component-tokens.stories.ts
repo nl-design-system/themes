738d6abac63859a9fe1a44fa90dac8015057077b
Robbert Broersma <git@robbertbroersma.nl>
2025-04-10 22:04:48 +0200

    docs: use new component pages for Mooi & Anders

--- packages/ma-design-tokens/documentation/component-tokens.stories.ts
+++ packages/ma-design-tokens/documentation/component-tokens.stories.ts
@@ -1,29 +1,29 @@
 import { ComponentStories } from '@nl-design-system-unstable/theme-toolkit/src/ComponentStories';
 import type { Meta, StoryObj } from '@storybook/react';
+import tokensDefinition from '@nl-design-system-community/design-tokens-definition/dist/list.json';
 import config from '../src/config.json';
 import tokens from '../dist/list.json';
 
 const meta = {
   title: 'Mooi & Anders',
   component: ComponentStories,
-  parameters: { controls: { include: 'showAll' }, chromatic: { disableSnapshot: false } },
-  args: { config, showAll: false, tokens, displayDesignTokens: false },
+  parameters: { controls: { include: 'showAll' }, chromatic: { disableSnapshot: true } },
+  args: {
+    config,
+    showAll: false,
+    tokens,
+    tokensDefinition,
+    displayDesignTokens: true,
+  },
 } satisfies Meta<typeof ComponentStories>;
 
 type Story = StoryObj<typeof meta>;
 
 export default meta;
-export const Components: Story = {
-  parameters: { theme: `${config.prefix}-theme` },
-  args: {
-    start: 0,
-    end: 50,
-  },
-};
 
-export const ComponentsContinued: Story = {
-  parameters: { theme: `${config.prefix}-theme` },
+export const ComponentTokensLightMode: Story = {
+  name: 'Component Tokens',
   args: {
-    start: 51,
+    theme: `${config.prefix}-theme`,
   },
 };
