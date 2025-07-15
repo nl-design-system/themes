23f88d15b81367c817dc01a4f86220fa5fe2dffb
Menno van Slooten <menno.vanslooten@gmail.com>
2025-07-15 12:05:54 +0200

    feat(start-theme): add code to build start-theme

---
+++ packages/start-design-tokens/style-dictionary.config.mjs
@@ -0,0 +1,42 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+import { typeDtcgDelegate } from 'style-dictionary/utils';
+import { readFile } from 'node:fs/promises';
+import { createConfig } from '../../style-dictionary-config.js';
+
+const build = async () => {
+  const themeConfig = JSON.parse(await readFile('./src/config.json', 'utf-8'));
+  StyleDictionary.registerPreprocessor({
+    name: 'dtcg-delegate',
+    preprocessor: typeDtcgDelegate,
+  });
+
+  register(StyleDictionary, {
+    excludeParentKeys: true,
+  });
+
+  let sd = new StyleDictionary({
+    ...createConfig({
+      selector: `.${themeConfig.prefix}-theme`,
+    }),
+    preprocessors: ['tokens-studio', 'dtcg-delegate'],
+    source: ['figma/**/start.tokens.json'],
+  });
+
+  await sd.cleanAllPlatforms();
+  await sd.buildAllPlatforms();
+
+  sd = new StyleDictionary({
+    ...createConfig({
+      className: `${themeConfig.prefix}-theme--color-scheme-dark`,
+      buildPath: 'dist/color-scheme-dark/',
+    }),
+    preprocessors: ['tokens-studio', 'dtcg-delegate'],
+    source: ['figma/**/start.tokens.json', 'figma/**/color-scheme-dark.tokens.json'],
+  });
+
+  await sd.cleanAllPlatforms();
+  await sd.buildAllPlatforms();
+};
+
+build();
