dc33fde58a906a4717950eff925505ab42942639
Charlotte <42849307+eslook@users.noreply.github.com>
2025-07-18 15:05:35 +0200

    feat: add amsterdam page header to themes and visual regression test (#1073)

---
+++ packages/voorbeeld-design-tokens/documentation/page-header/ams-page-header.stories.tsx
@@ -0,0 +1,98 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PageHeader, Grid, LinkList, Heading } from '@amsterdam/design-system-react';
+
+const defaultStoryLinks = [
+  [
+    { href: '#', label: 'Kaart' },
+    { href: '#', label: 'Panoramabeelden' },
+    { href: '#', label: 'Tabellen' },
+    { href: '#', label: 'Catalogus (Beta)' },
+    { href: '#', label: 'Kaarten' },
+    { href: '#', label: 'Datacatalogus' },
+  ],
+  [
+    { href: '#', label: 'Over de organisatie' },
+    { href: '#', label: 'Over het dataplatform' },
+  ],
+  [
+    { href: '#', label: 'Help' },
+    { href: '#', label: 'Contact' },
+  ],
+];
+
+const meta = {
+  id: 'ams-page-header',
+  title: 'Components/Page Header/Amsterdam',
+  component: PageHeader,
+  parameters: { actions: { disable: true } },
+  args: {
+    brandName: 'Data Amsterdam',
+    children: (
+      <Grid paddingVertical="large">
+        <PageHeader.GridCellNarrowWindowOnly span="all">
+          <LinkList>
+            <LinkList.Link href="#" lang="en">
+              English
+            </LinkList.Link>
+          </LinkList>
+        </PageHeader.GridCellNarrowWindowOnly>
+        <Grid.Cell span={4}>
+          <Heading className="ams-mb-s" level={3}>
+            Onderdelen
+          </Heading>
+          <LinkList>
+            {defaultStoryLinks[0].map(({ href, label }) => (
+              <LinkList.Link href={href} key={label}>
+                {label}
+              </LinkList.Link>
+            ))}
+          </LinkList>
+        </Grid.Cell>
+        <Grid.Cell span={4}>
+          <Heading className="ams-mb-s" level={3}>
+            Over ons
+          </Heading>
+          <LinkList>
+            {defaultStoryLinks[1].map(({ href, label }) => (
+              <LinkList.Link href={href} key={label}>
+                {label}
+              </LinkList.Link>
+            ))}
+          </LinkList>
+        </Grid.Cell>
+        <Grid.Cell span={4}>
+          <Heading className="ams-mb-s" level={3}>
+            Help
+          </Heading>
+          <LinkList>
+            {defaultStoryLinks[2].map(({ href, label }) => (
+              <LinkList.Link href={href} key={label}>
+                {label}
+              </LinkList.Link>
+            ))}
+          </LinkList>
+        </Grid.Cell>
+      </Grid>
+    ),
+    menuItems: [
+      <PageHeader.MenuLink href="#" key={1} lang="en">
+        English
+      </PageHeader.MenuLink>,
+      <PageHeader.MenuLink fixed href="#" key={2}>
+        Zoeken
+      </PageHeader.MenuLink>,
+    ],
+  },
+} satisfies Meta<typeof PageHeader>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+export const AmsterdamTheme: Story = {
+  name: 'Amsterdam theme',
+  parameters: { theme: 'ams-theme' },
+};
