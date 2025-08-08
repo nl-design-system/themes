a2e61c630c51349a50f06ffb5e5faa0858f49791
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-08-08 15:02:33 +0200

    feat: add utrecht form field label to themes and visual regression test
    As part of https://github.com/nl-design-system/kernteam/issues/1514

---
+++ packages/voorbeeld-design-tokens/documentation/form-field-label/utrecht-form-field-label.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FormLabel as FormFieldLabel } from '@utrecht/component-library-react/dist/css-module';
+
+const meta = {
+  id: 'utrecht-form-field-label',
+  title: 'Components/Form Field Label/Utrecht',
+  component: FormFieldLabel,
+  parameters: { actions: { disable: true } },
+  args: {
+    for: 'idref',
+    children: 'Label',
+  },
+} satisfies Meta<typeof FormFieldLabel>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+
+export const UtrechtTheme: Story = {
+  name: 'Utrecht theme',
+  parameters: { theme: 'utrecht-theme' },
+};
