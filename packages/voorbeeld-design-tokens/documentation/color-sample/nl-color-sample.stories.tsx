f44900bfa502a538d657c43e71ab3a8d5672a6b4
Roy Honders <rhonders@quintor.nl>
2025-10-24 13:04:43 +0200

    feat: add color sample

---
+++ packages/voorbeeld-design-tokens/documentation/color-sample/nl-color-sample.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ColorSample } from '@nl-design-system-candidate/color-sample-react/css';
+
+const meta = {
+  id: 'nl-color-sample',
+  title: 'Components/Color Sample/Candidate',
+  component: ColorSample,
+  args: {
+    color: '#007DAD',
+  },
+} satisfies Meta<typeof ColorSample>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+
+export const StartTheme: Story = {
+  name: 'Start theme',
+  parameters: { theme: 'start-theme' },
+};
