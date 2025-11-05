b1e9abd2894d2426315335c4888d1ddd992cfa70
Roy Honders <rhonders@quintor.nl>
2025-11-05 16:29:54 +0100

    Shortened text & added lead to lead variant

--- packages/theme-toolkit/src/component-stories-candidate.tsx
+++ packages/theme-toolkit/src/component-stories-candidate.tsx
@@ -335,15 +335,7 @@ export const CANDIDATE_COMPONENT_STORIES: ComponentStory[] = [
     storyId: 'react-nl-paragraph--default',
     group: STORY_GROUPS['PARAGRAPH'],
     name: 'Candidate Paragraph',
-    render: () => (
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum
-      </Paragraph>
-    ),
+    render: () => <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Paragraph>,
     detectTokens: {
       anyOf: [
         'nl.paragraph.color',
@@ -361,15 +353,7 @@ export const CANDIDATE_COMPONENT_STORIES: ComponentStory[] = [
     component: 'nl-paragraph',
     group: STORY_GROUPS['PARAGRAPH'],
     name: 'Candidate Lead Paragraph',
-    render: () => (
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum
-      </Paragraph>
-    ),
+    render: () => <Paragraph purpose="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Paragraph>,
     detectTokens: {
       anyOf: [
         'nl.paragraph.lead.color',
