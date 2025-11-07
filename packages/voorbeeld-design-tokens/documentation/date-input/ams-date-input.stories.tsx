c5fee0e91a75b91c70c8035416567602f97e0f99
Charlotte <42849307+eslook@users.noreply.github.com>
2025-11-07 16:11:46 +0100

    docs: add date input to themes and visual regression (#1198)
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

---
+++ packages/voorbeeld-design-tokens/documentation/date-input/ams-date-input.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DateInput } from '@amsterdam/design-system-react';
+
+const meta = {
+  id: 'ams-date-input',
+  title: 'Components/Date Input/Amsterdam',
+  component: DateInput,
+  parameters: { actions: { disable: true } },
+  args: {},
+  argTypes: {},
+} satisfies Meta<typeof DateInput>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+
+export const AmsterdamTheme: Story = {
+  name: 'Amsterdam theme',
+  parameters: { theme: 'ams-theme' },
+};
