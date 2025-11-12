254fc2ccde04acc3e6617e129cf2e7b4306f931c
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2025-11-12 14:57:37 +0100

    fix: load missing fonts

---
+++ proprietary/hoeksche-waard-design-tokens/src/font.js
@@ -0,0 +1,5 @@
+import '@fontsource/fira-code/400.css';
+import '@fontsource/fira-sans/400.css';
+import '@fontsource/fira-sans/700.css';
+import '@fontsource/source-serif-pro/400.css';
+import '@fontsource/source-serif-pro/700.css';
