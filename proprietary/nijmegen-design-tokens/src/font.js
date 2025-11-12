bb9fa36a48767f3bed43d687b980c0a7850533db
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2025-11-12 14:57:37 +0100

    fix: load missing fonts

--- proprietary/nijmegen-design-tokens/src/font.js
+++ proprietary/nijmegen-design-tokens/src/font.js
@@ -1,5 +1,9 @@
+import '@fontsource/fira-sans/400.css';
+import '@fontsource/fira-sans/700.css';
 import '@fontsource/source-sans-pro/300.css';
 import '@fontsource/source-sans-pro/400.css';
 import '@fontsource/source-sans-pro/400-italic.css';
 import '@fontsource/source-sans-pro/600.css';
 import '@fontsource/source-sans-pro/700.css';
+import '@fontsource/source-serif-pro/400.css';
+import '@fontsource/source-serif-pro/700.css';
