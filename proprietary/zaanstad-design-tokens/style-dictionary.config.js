4f7dce4ac769453250812fce6be5a2f1d8846bce
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2025-11-14 15:52:25 +0100

    chore: remove unused file
    This file was never in use: style-dictionary.mjs refers to the root style-dictionary.config.js as intended. Since it was never in use, nothing changes, no changeset is needed.

--- proprietary/zaanstad-design-tokens/style-dictionary.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('./src/config.json');
-const { createConfig } = require('../../style-dictionary-config');
-
-module.exports = createConfig({
-  backwardsCompatible: true,
-  selector: `.${config.prefix}-theme`,
-});
