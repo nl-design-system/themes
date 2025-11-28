a944263c663a7e4980cf2c0e092997f38e0acd1f
Charlotte <42849307+eslook@users.noreply.github.com>
2025-11-28 16:37:57 +0100

    docs: add ams file input storybook and visual regression (#1235)
    As part of "Component beschikbaar voor visuele regressietests" in
    "Stappenplan: Community voor organisaties".
    
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

---
+++ packages/voorbeeld-design-tokens/documentation/file-input/ams-file-input.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FileInput } from '@amsterdam/design-system-react';
+
+const meta = {
+  id: 'ams-file-input',
+  title: 'Components/File Input/Amsterdam',
+  component: FileInput,
+  parameters: { actions: { disable: true } },
+  args: {},
+} satisfies Meta<typeof FileInput>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const VoorbeeldTheme: Story = {
+  name: 'Voorbeeld theme',
+  parameters: { theme: 'voorbeeld-theme' },
+};
+
+export const AmsterdamTheme: Story = {
+  name: 'Amsterdam theme',
+  parameters: { theme: 'ams-theme' },
+};
