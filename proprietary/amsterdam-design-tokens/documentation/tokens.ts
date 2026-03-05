f2295f56b5b5ef04bba152fa6b5ad34d384541d6
Marwa <marwahello@frameless.io>
2026-03-05 08:34:26 +0100

    refactor: remove amsterdam-design-tokens from proprietary

--- proprietary/amsterdam-design-tokens/documentation/tokens.ts
+++
@@ -1,7 +0,0 @@
-import valueTree from '@amsterdam/design-system-tokens/dist/index.json';
-import { convertValueTreeToDesignTokenTree } from '@nl-design-system-unstable/tokens-lib/dist/design-tokens';
-import { addPath, treeToArray } from '@nl-design-system-unstable/tokens-lib/dist/ExampleTokensCSS';
-
-export const tokens = addPath(convertValueTreeToDesignTokenTree(valueTree));
-
-export const tokensList = treeToArray(tokens);
