f064d207200ef86099bb6db151ec2b92fc4f2ad2
Charlotte Gieltjes <charlotte.gieltjes@rebels.io>
2026-03-13 12:41:33 +0100

    chore: get accurate ui test before we continue

--- proprietary/groningen-design-tokens/documentation/components.stories.ts
+++ proprietary/groningen-design-tokens/documentation/components.stories.ts
@@ -1,13 +1,13 @@
 import { ComponentStories } from '@nl-design-system-unstable/theme-toolkit/src/ComponentStories';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import config from '../src/config.json';
-import tokens from '../dist/list.json';
+// TODO: import tokens from '../dist/list.json';
 
 const meta = {
   title: 'Groningen',
   component: ComponentStories,
   parameters: { controls: { include: 'showAll' }, chromatic: { disableSnapshot: false } },
-  args: { config, showAll: false, tokens, displayDesignTokens: false },
+  args: { config, showAll: false, displayDesignTokens: false },
 } satisfies Meta<typeof ComponentStories>;
 
 type Story = StoryObj<typeof meta>;
