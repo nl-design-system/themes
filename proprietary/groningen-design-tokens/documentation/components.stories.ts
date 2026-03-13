c28583cb0fd2527404972594cfa5176947ea4db4
Charlotte <42849307+eslook@users.noreply.github.com>
2026-03-13 11:50:32 +0100

    chore: split groningen component stories (#1372)
    Prepares for https://github.com/nl-design-system/themes/pull/1342
    
    No actual changes to the theme, just splits the stories.
    
    Co-authored-by: Charlotte Gieltjes <charlotte.gieltjes@rebels.io>

--- proprietary/groningen-design-tokens/documentation/components.stories.ts
+++ proprietary/groningen-design-tokens/documentation/components.stories.ts
@@ -6,10 +6,38 @@ const meta = {
   title: 'Groningen',
   component: ComponentStories,
   parameters: { controls: { include: 'showAll' }, chromatic: { disableSnapshot: false } },
-  args: { config, showAll: false },
+  args: { config, showAll: false, displayDesignTokens: false },
 } satisfies Meta<typeof ComponentStories>;
 
 type Story = StoryObj<typeof meta>;
 
 export default meta;
-export const Components: Story = { parameters: { theme: `${config.prefix}-theme` } };
+
+// Chromatic has a limit of 25000 pixels per story, so we need to split them
+// into multiple stories.
+
+export const ComponentsPage1: Story = {
+  name: 'Visual regression test (component 1 - 50)',
+  args: {
+    theme: `${config.prefix}-theme`,
+    start: 0,
+    end: 50,
+  },
+};
+
+export const ComponentsPage2: Story = {
+  name: 'Visual regression test (component 51 - 100)',
+  args: {
+    theme: `${config.prefix}-theme`,
+    start: 51,
+    end: 100,
+  },
+};
+
+export const ComponentsPage3: Story = {
+  name: 'Visual regression test (component 101 - ∞)',
+  args: {
+    theme: `${config.prefix}-theme`,
+    start: 101,
+  },
+};
